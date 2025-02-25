33fd8c22d03d1597e8228c9b560c715bed631313
Jingping Yin <yinjingping2022@gmail.com>
2025-02-25 18:04:49 +0800

    fix: koala poseidon2

--- crates/core/machine/src/operations/poseidon2/air.rs
+++ crates/core/machine/src/operations/poseidon2/air.rs
@@ -9,6 +9,25 @@ use zkm2_stark::air::MachineAirBuilder;
 
 use super::{permutation::Poseidon2Cols, NUM_EXTERNAL_ROUNDS, NUM_INTERNAL_ROUNDS, WIDTH};
 
+const INTERNAL_DIAG_MONTY_16: [KoalaBear; 16] = KoalaBear::new_array([
+    KoalaBear::ORDER_U32 - 2,
+    1,
+    2,
+    (KoalaBear::ORDER_U32 + 1) >> 1,
+    3,
+    4,
+    (KoalaBear::ORDER_U32 - 1) >> 1,
+    KoalaBear::ORDER_U32 - 3,
+    KoalaBear::ORDER_U32 - 4,
+    KoalaBear::ORDER_U32 - ((KoalaBear::ORDER_U32 - 1) >> 8),
+    KoalaBear::ORDER_U32 - ((KoalaBear::ORDER_U32 - 1) >> 3),
+    KoalaBear::ORDER_U32 - 127,
+    (KoalaBear::ORDER_U32 - 1) >> 8,
+    (KoalaBear::ORDER_U32 - 1) >> 3,
+    (KoalaBear::ORDER_U32 - 1) >> 4,
+    127,
+]);
+
 pub fn apply_m_4_mut<AF>(x: &mut [AF])
 where
     AF: FieldAlgebra,
@@ -43,24 +62,6 @@ pub fn external_linear_layer<AF: FieldAlgebra + Copy>(state: &[AF; WIDTH]) -> [A
 }
 
 pub fn internal_linear_layer_mut<F: FieldAlgebra>(state: &mut [F; WIDTH]) {
-    const INTERNAL_DIAG_MONTY_16: [KoalaBear; 16] = KoalaBear::new_array([
-        KoalaBear::ORDER_U32 - 2,
-        1,
-        2,
-        (KoalaBear::ORDER_U32 + 1) >> 1,
-        3,
-        4,
-        (KoalaBear::ORDER_U32 - 1) >> 1,
-        KoalaBear::ORDER_U32 - 3,
-        KoalaBear::ORDER_U32 - 4,
-        KoalaBear::ORDER_U32 - ((KoalaBear::ORDER_U32 - 1) >> 8),
-        KoalaBear::ORDER_U32 - ((KoalaBear::ORDER_U32 - 1) >> 3),
-        KoalaBear::ORDER_U32 - 127,
-        (KoalaBear::ORDER_U32 - 1) >> 8,
-        (KoalaBear::ORDER_U32 - 1) >> 3,
-        (KoalaBear::ORDER_U32 - 1) >> 4,
-        127,
-    ]);
     let matmul_constants: [<F as FieldAlgebra>::F; WIDTH] =
         INTERNAL_DIAG_MONTY_16
             .iter()
@@ -92,7 +93,6 @@ where
 
     // Apply the sboxes.
     // See `populate_external_round` for why we don't have columns for the sbox output here.
-    let mut sbox_deg_7: [AB::Expr; WIDTH] = core::array::from_fn(|_| AB::Expr::ZERO);
     let mut sbox_deg_3: [AB::Expr; WIDTH] = core::array::from_fn(|_| AB::Expr::ZERO);
     for i in 0..WIDTH {
         let calculated_sbox_deg_3 = add_rc[i].clone() * add_rc[i].clone() * add_rc[i].clone();
@@ -103,12 +103,10 @@ where
         } else {
             sbox_deg_3[i] = calculated_sbox_deg_3;
         }
-
-        sbox_deg_7[i] = sbox_deg_3[i].clone() * sbox_deg_3[i].clone() * add_rc[i].clone();
     }
 
     // Apply the linear layer.
-    let mut state = sbox_deg_7;
+    let mut state = sbox_deg_3;
     external_linear_layer_mut(&mut state);
 
     let next_state = if r == (NUM_EXTERNAL_ROUNDS / 2) - 1 {
@@ -144,13 +142,9 @@ where
             sbox_deg_3 = internal_sbox[r].into();
         }
 
-        // See `populate_internal_rounds` for why we don't have columns for the sbox output
-        // here.
-        let sbox_deg_7 = sbox_deg_3.clone() * sbox_deg_3.clone() * add_rc.clone();
-
         // Apply the linear layer.
         // See `populate_internal_rounds` for why we don't have columns for the new state here.
-        state[0] = sbox_deg_7.clone();
+        state[0] = sbox_deg_3.clone();
         internal_linear_layer_mut(&mut state);
 
         if r < NUM_INTERNAL_ROUNDS - 1 {
