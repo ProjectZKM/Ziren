e1fe7677124e8c26565047230f9b9803b658906c
Jingping Yin <yinjingping2022@gmail.com>
2025-04-14 14:41:35 +0800

    chore: remove e2e-test features

--- crates/verifier/src/tests.rs
+++ crates/verifier/src/tests.rs
@@ -7,7 +7,6 @@ use zkm_sdk::{HashableKey, ProverClient, ZKMStdin};
 
 // RUST_LOG=debug cargo test -r test_verify_groth16 --features ark
 #[test]
-#[cfg(feature = "e2e-test")]
 fn test_verify_groth16() {
     // Set up the pk and vk.
     let client = ProverClient::cpu();
@@ -39,7 +38,6 @@ fn test_verify_groth16() {
 }
 
 #[test]
-#[cfg(feature = "e2e-test")]
 fn test_verify_plonk() {
     // Set up the pk and vk.
     let client = ProverClient::cpu();
