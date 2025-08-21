02eff8a2c1b96462fba0822a1539a011d126b0ae
eigmax <sd@eigen.market>
2025-08-21 16:47:57 +0800

    docs: update comments

--- crates/build/src/command/local.rs
+++ crates/build/src/command/local.rs
@@ -24,7 +24,7 @@ pub(crate) fn create_local_command(
     // 4. Remove the rustc configuration, otherwise in a build script it will attempt to compile the
     //    program with the toolchain of the normal build process, rather than the Ziren toolchain.
 
-    // TODO: Remove this conditional compilation once we provide it in ziren toolchain. 
+    // TODO: Remove this conditional compilation once we provide it in ziren toolchain.
     // See docs.zkm.io/introduction/installation.html#use-musl-gcc-for-static-compilation
     #[cfg(target_os = "linux")]
     command
