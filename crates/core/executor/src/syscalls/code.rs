VanhGer/zkMIPS

feat: add base structure for aes128 precompile

crates/core/executor/src/syscalls/code.rs
@@ -144,6 +144,9 @@ pub enum SyscallCode {
     /// Executes the `POSEIDON2_PERMUTE` precompile.
     POSEIDON2_PERMUTE = 0x00_00_01_30,
 
+    /// Executes the `AES128_ENCRYPT` precompile.
+    AES128_ENCRYPT = 0x00_01_01_31,
+
     UNIMPLEMENTED = 0xFF_FF_FF_FF,
 }
 
@@ -189,6 +192,7 @@ impl SyscallCode {
             0x00_01_01_29 => SyscallCode::BN254_FP2_ADD,
             0x00_01_01_2A => SyscallCode::BN254_FP2_SUB,
             0x00_01_01_2B => SyscallCode::BN254_FP2_MUL,
+            0x00_01_01_31 => SyscallCode::AES128_ENCRYPT,
             0x00_00_01_1C => SyscallCode::BLS12381_DECOMPRESS,
             0x00_01_01_2C => SyscallCode::SECP256R1_ADD,
             0x00_00_01_2D => SyscallCode::SECP256R1_DOUBLE,

ProjectZKM/Ziren

crates/core/executor/src/syscalls/code.rs
@@ -185,6 +185,9 @@ pub enum SyscallCode {
 
     SYS_LINUX = 4000, // not real syscall, used for represent all linux syscalls
 
+    /// Executes the `AES128_ENCRYPT` precompile.
+    AES128_ENCRYPT = 0x01_01_00_31,
+
     UNIMPLEMENTED = 0xFF_FF_FF_FF,
 }
 
@@ -206,6 +209,7 @@ impl SyscallCode {
             0x01_01_00_07 => SyscallCode::ED_ADD,
             0x00_01_00_08 => SyscallCode::ED_DECOMPRESS,
             0x01_01_00_09 => SyscallCode::KECCAK_SPONGE,
+            0x01_01_00_31 => SyscallCode::AES128_ENCRYPT,
             0x01_01_00_0A => SyscallCode::SECP256K1_ADD,
             0x00_01_00_0B => SyscallCode::SECP256K1_DOUBLE,
             0x00_01_00_0C => SyscallCode::SECP256K1_DECOMPRESS,